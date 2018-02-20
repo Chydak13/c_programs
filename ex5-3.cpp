d57e56fb91a0b858f43b3a9fc5ddf9c68ca7d0f0
Vlad Korotkevich <chydak13@yandex.ru>
2018-02-20 16:38:05 +0300

    modified ex5-3

--- ex5-3.cpp
+++ ex5-3.cpp
@@ -6,4 +6,8 @@ int main()
   unsigned short UShortValue = 65535;
   cout << "Incrementing unsigned short " << UShortValue << " gives: ";
   cout << ++UShortValue << endl;
+
+  short SShortValue = 32767;
+  cout << "Incrementing signed short " << SShortValue << " gives: ";
+  cout << ++SShortValue << endl;
 }
