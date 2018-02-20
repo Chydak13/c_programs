49bfe42a5386a5aa699cd6fcf92fa22eff535384
Vlad Korotkevich <chydak13@yandex.ru>
2018-02-20 16:15:00 +0300

    .

---
+++ ex5-3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  unsigned short UShortValue = 65535;
+  cout << "Incrementing unsigned short " << UShortValue << " gives: ";
+  cout << ++UShortValue << endl;
+}
